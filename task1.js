7ae5c0626a28b6a8c8e4b5a1b49eee8ef682a739
vitalina26 <73598254+vitalina26@users.noreply.github.com>
2022-09-12 20:28:36 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,33 +1,42 @@
 function loadGrain(levels) {
-    if(levels.length < 3 )
-      return 0;
-    let count = 0;
-    let maxHeight = levels[0];
-    let maxHeightIndex = 0;
-    let tempCount = levels[1];
-    for(let i = 1; i < levels.length - 1; i++){
-      if(maxHeight <= levels[i]){
-          maxHeight = levels[i];
-          maxHeightIndex = i;
-          if(i!= levels.length - 1 )
-            tempCount = levels[i+1];
-      }
-      else if(i != levels.length - 1){
-        if(levels[i+1] > levels[i]){
-          if(maxHeight < levels[i+1]){
-              count += (maxHeight) * (i - maxHeightIndex) - tempCount
-          }
-          else{
-              count += (levels[i+1]) * (i - maxHeightIndex) - tempCount
-              
-          tempCount += levels[i+1] + count;
-          }
+        let maxHeight = levels[0];
+        let maxHeightIndex = 0;
+        let count = 0;
+        let tempCount = 0;
+        for (let i = 1; i <= levels.length - 1; i++) {
+          if (levels[i] >= maxHeight) {
+               maxHeight = levels[i];
+                maxHeightIndex = i;
+                tempCount = 0;
+            }
+            else {
+                count += maxHeight - levels[i];
+                tempCount += maxHeight - levels[i];
+            }
         }
-        else{
-          tempCount += levels[i+1];
-        
+        if (maxHeightIndex < levels.length - 1) {
+  
+            count -= tempCount;
+            maxHeight = levels[levels.length - 1];
+            for (let i = levels.length - 1; i >= maxHeightIndex; i--) {
+                if (levels[i] >= maxHeight) {
+                    maxHeight = levels[i];
+                }
+                else {
+                    count += maxHeight - levels[i];
+                }
+            }
         }
-      }
-    }
-    return count;
+        return count;
   }
+
+console.log(loadGrain([4, 1, 3]));//2
+console.log(loadGrain([2, 1, 5, 2, 7, 4, 10]));//7
+console.log(loadGrain([2, 0, 1, 5, 2, 7]));//6
+console.log(loadGrain([2, 4, 2]) );//0
+console.log(loadGrain([7, 4]) );//0
+console.log(loadGrain([]));//0
+console.log(loadGrain([5, 1, 1, 3, 1, 1, 5]));//18
+console.log(loadGrain([3, 2, 0, 1]));//1
+console.log(loadGrain([15, 0, 6, 10, 11, 2, 5]));//20
+console.log(loadGrain([46,37,2,15,33,21,8,30,18,19,24,0,31,3,39]));//266
